cefe031c5f26e0537d0815b93a282431a2aa747b
IsmHernandez11 <ismasnayhernandez2010@gmail.com>
2026-01-09 15:13:06 -0500

    cmabio de botones

--- Ejercicio git/ejercicio.js
+++ Ejercicio git/ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
+    let cmpImagenSaludo
+    cmpImagenSaludo=document.getElementById(imgSaludo);
+    cmpImagenSaludo.src="./imagenes/9unC.gif"
     console.log("ingresar saludar");
+
 }

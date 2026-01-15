3fdb0c970e6687f20404234d665852c8d3853115
IsmHernandez11 <ismasnayhernandez2010@gmail.com>
2026-01-14 21:35:27 -0500

    aadi utilitarios y recuperar float e int

--- saludo.js
+++ saludo.js
@@ -1,14 +1,11 @@
-saludar = function () {
-    //recuperar el valor de la caja de texto txtNombre
-    let nombre = recuperarTexto("txtNombre");
-    //recuperar el valor de la caja de texto txtApellido
-    let apellido = recuperarTexto("txtApellido");
-}
-
-recuperarTexto = function (idComponente) {
-    let componente;
-    let valorIngresado;
-    componente = document.getElementById(idComponente);
-    valorIngresado = componente.value;
-    return valorIngresado;
+saludar=function(){
+    let nombre;
+    nombre=recuperarTexto("txtNombre");
+    let apellido=recuperarTexto("txtApellido")
+    let edad=recuperarInt("txtEdad");
+    let estatura=recuperarFloat("txtEstatura");
+    let mensajeBienvenida="Bienvenido "+nombre+" "+apellido;
+    mostrarTexto("lblResultado",mensajeBienvenida);
+    mostrarImagen("imgSaludo","./imagenes/panda.png");
+    mostrarTextoEnCaja("txtNombre","");
 }
